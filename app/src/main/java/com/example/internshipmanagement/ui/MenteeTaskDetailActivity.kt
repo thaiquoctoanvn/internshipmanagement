31a3534481d840fa899509c1522dc66efe675e8c
Thai Quoc Toan <52631512+thaiquoctoanvn@users.noreply.github.com>
2020-11-22 14:17:56 +0700

    feat - prepare for mentee task function

---
+++ app/src/main/java/com/example/internshipmanagement/ui/MenteeTaskDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.internshipmanagement.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenteeTaskDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mentee_task_detail)
+    }
+}
